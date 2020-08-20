a8a685989acba4e9754b6956e39d0bde8ffb5691
jdpalaciob <jdpalaciob@gmail.com>
2020-08-20 11:59:28 -0500

    Se crea la funcion para insertar nuevos records a la tabla. Se presentaron problemas con el ingreso de valores tipo String asociados a lo literal que debe ser la instruccion SQL. La solucion fue implementar la introduccion de los parametros mediante el operador "%", pero en lugar de concatenar el tuple de parametros con el mismo operador, se separaron con una coma (","). Se entiende esto como una practica mas segura, ademas de permitir la versatilidad de los tipos de datos sin mayor complieccion. Especificamente, esta forma de string-formatting permitia el uso del tipo "None" como valor predeterminado en las funciones, dejando el codigo preparado para posibles datos no ingresados por el usuario. Se emplean solo keyword arguments, blindandose contra el mismo problema. La creacion de tabla se actualiza a columnas con significado real, tipo de datos definidos, restricciones aplicadas y valores default.

--- postgres_db.py
+++ postgres_db.py
@@ -2,15 +2,51 @@
 import psycopg2 as psql
 
 def stablish_connection(data_base, user_name=None, password=None):
-    conn = psql.connect(dbname=data_base, user=user_name, password=password, host='localhost', port=5432)
+    """ Stablish connection and creates table """
+
+    conn = psql.connect(
+        dbname=data_base,
+        user=user_name,
+        password=password,
+        host='localhost',
+        port=5432
+        )
     cur = conn.cursor()
+
     cur.execute(
-        'CREATE TABLE IF NOT EXISTS \
-            reactives (id SERIAL PRIMARY KEY, name VARCHAR)'
+        'CREATE TABLE IF NOT EXISTS reactives (\
+            id SERIAL PRIMARY KEY,\
+            name VARCHAR(100) NOT NULL,\
+            chem_form VARCHAR(15),\
+            CAS_number VARCHAR(50) UNIQUE,\
+            nature CHAR(3),\
+            ph_nature VARCHAR(5),\
+            Quantity NUMERIC(6,2) DEFAULT 0\
+            );'
         )
     conn.commit()
     conn.close()
 
+def inserting_data(*, name, chem_formula=None, cas_number=None, nature=None, ph_nature=None,
+                   quantity=None, data_base, user_name=None, password=None):
+    """ Allows user to introduce records to the table in DB """
+
+    conn = psql.connect(
+        dbname=data_base,
+        user=user_name,
+        password=password,
+        host='localhost',
+        port=5432
+        )
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO reactives (\
+        name, chem_form, CAS_number, nature, ph_nature, Quantity)\
+        VALUES (%s,%s,%s,%s,%s,%s);", (name, chem_formula, cas_number, nature, ph_nature, quantity)
+        )
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
 
@@ -19,3 +55,14 @@ if __name__ == '__main__':
     PASWRD = 'plant123'
 
     stablish_connection(DB, USER, PASWRD)
+
+    NAME = 'Hydrochloric Acid'
+    FORMULA = 'HCl'
+    CAS = '7647-10-0'
+    NATURE = 'INO'
+    PH = 'acid'
+    QUANTITY = 12
+
+    inserting_data(name=NAME, chem_formula=NAME, cas_number=CAS, nature=NATURE, ph_nature=PH, quantity=QUANTITY,
+                   data_base=DB, user_name=USER, password=PASWRD)
+    

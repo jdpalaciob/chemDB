ee76cb6e61af58cfbd96324b62131c2b82f14177
jdpalaciob <jdpalaciob@gmail.com>
2020-08-22 13:09:55 -0500

    Se crea una funcion prueba para conectarse a la base de datos y crear la tabla correspondiente. Los parametros necesarios para la conexion se manejaron en la URL de la forma ?key=value. En la funcion dummy se comprueba que es necesario un comando por cada key que desee ser ingresado.

--- flask_block.py
+++ flask_block.py
@@ -1,13 +1,30 @@
-""" Script to develop the API REST block of chemDB """
+""" Script to develop the API REST block of chemDB
 
-from flask import Flask
+DB = 'chemDB'
+USER = 'plant_chief'
+PASWRD = 'plant123'
+"""
+
+from flask import Flask, request
+import postgres_db as pdb
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def homepage(name):
-    return f'Hello {name}'
+@app.route('/')
+def homepage():
+    # parameters in the form ?key=value
+    name = request.args.get('name')
+    last = request.args.get('last')
+    return f'Hello {name} {last}'
+
+@app.route('/chemDB')
+def connection():
+    DB = request.args.get('db')
+    USER = request.args.get('user')
+    PASWRD = request.args.get('pword')
+    pdb.stablish_connection(data_base=DB, user_name=USER, password=PASWRD)
+    return 'Access to Data Base granted'
 
 if __name__ == "__main__":
-    app.debug = False
+    app.debug = True
     app.run()

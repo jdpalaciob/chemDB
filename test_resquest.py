5b5ee77befcacf5a36362781a23a6686564cde1d
jdpalaciob <jdpalaciob@gmail.com>
2020-08-24 15:40:13 -0500

    El script "test_request.py" es organizado y se deja un ejemplo completo para cada tipo de request.

--- test_resquest.py
+++ test_resquest.py
@@ -1,30 +1,32 @@
+""" Scirpt to easily formulate request to the local postgres data base """
+
 import requests
 
 
 BASE_URL = 'http://127.0.0.1:5000/'
 
-response = requests.get(BASE_URL + 'chemDB')
+creating = requests.get(BASE_URL + 'chemDB')
 
-# response = requests.post(BASE_URL + 'chemDB/insert', json={
-#                         "chem_id":"CAC-001",
-#                         "name":"Sulfuric Acid",
-#                         "chem_formula":"H2SO4",
-#                         "cas_number":"7664-93-9",
-#                         "nature":"INO",
-#                         "ph_nature":"acid",
-#                         "quantity":28}
-#                         )
-# response = requests.post(BASE_URL + 'chemDB/insert', json={
-#                         "chem_id":"MAN-002",
-#                         "name":"Sodium Hydroxide",
-#                         "nature":"INO",
-#                         "ph_nature":"base",
-#                         "quantity":25}
-#                         )
+inserting = requests.post(BASE_URL + 'chemDB/insert', json={
+                        "chem_id":"CAC-001",
+                        "name":"Sulfuric Acid",
+                        "chem_formula":"H2SO4",
+                        "cas_number":"7664-93-9",
+                        "nature":"INO",
+                        "ph_nature":"acid",
+                        "quantity":28}
+                        )
 
-response = requests.put(BASE_URL + 'chemDB/update', json={
+updating = requests.put(BASE_URL + 'chemDB/update', json={
                         "chem_id":"MAN-002",
                         "chem_formula":"NaOH",
                         "cas_number":"1310-73-2",
                         "quantity":15}
                         )
+
+deleting = requests.delete(BASE_URL + 'chemDB/delete', json={
+                            "chem_id":"MAN-002"}
+                        )
+
+searching = requests.get(BASE_URL + 'chemDB/info' + "?view=*&where=chem_id&equals=MAN-001")
+# print((searching.content).decode())

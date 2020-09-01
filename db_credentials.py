486eafd9f97a5590f9e8d1a120cfb37aae6f8f91
jdpalaciob <jdpalaciob@gmail.com>
2020-09-01 15:14:58 -0500

    Se añaden mas credenciales de conexion.
    El script "db_credentials.py" recibe nuevas credenciales de conexion. Se introducen parametros necesarios para la conexion mediante la ORM de SQLAlchemy

--- db_credentials.py
+++ db_credentials.py
@@ -1,5 +1,7 @@
 credentials = {
     'data_base' : 'chemDB',
     'user_name' : 'plant_chief',
-    'password'  : 'plant123'
+    'password'  : 'plant123',
+    'port'      : 5432,
+    'host'      : 'localhost'
 }

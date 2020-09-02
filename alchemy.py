59253e35947b5feb7e192d5e63d824ac61e0c145
jdpalaciob <jdpalaciob@gmail.com>
2020-09-02 10:26:41 -0500

    Actualizacion de datos con SQLaclhemy.
    Se construye la funcion "update_data" para actualizar records en la tabla. Los datos deben ser introducidos como una instancia de la misma, es decir, una instancia de la clase Reactive; adicional se requiere el chem_id del reactivo a actualizar. Se corrigen errores tipograficos.

--- alchemy.py
+++ alchemy.py
@@ -16,7 +16,7 @@ engine = create_engine(f'postgresql://{USER}:{PASWRD}@{HOST}:{PORT}/{DB}', echo=
 
 Base = declarative_base()
 
-class Reactvie(Base):
+class Reactive(Base):
     __tablename__ = 'reactives'
 
     chem_id = Column(String(7), primary_key=True)
@@ -36,14 +36,48 @@ def insert_data(data, engine):
     session.add(data)
     session.commit()
 
+def update_data(chem_id, new_data, engine):
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    chem = session.query(Reactive).filter_by(chem_id=chem_id).first()
+
+    if chem:
+        if new_data.name:
+            chem.name = new_data.name
+        if new_data.chem_form:
+            chem.chem_form = new_data.chem_form
+        if new_data.cas_number:
+            chem.cas_number = new_data.cas_number
+        if new_data.nature:
+            chem.nature = new_data.nature
+        if new_data.ph_nature:
+            chem.ph_nature = new_data.ph_nature
+        if new_data.quantity:
+            chem.quantity = new_data.quantity
+    
+        return session.commit()
+        # return f'The reactvie with chem_id = {chem_id} has been updated'
+    else:
+        # return f'The reactive with chem_id = {chem_id} is not registered'
+        raise NameError(f'The reactive with chem_id = {chem_id} is not registered')
+    
+
 if __name__ == '__main__':
 
     # ADD
-    reactive_1 = Reactvie(chem_id='MAN-001',
+    reactive_1 = Reactive(chem_id='MAN-001',
                           name='Hydrochloric Acid',
                           chem_form='HCl',
                           cas_number='7647-01-0',
                           nature='INO',
                           ph_nature='acid',
                           quantity=12)
-    insert_data(reactive_1, engine)
+    # insert_data(reactive_1, engine)
+    
+    # UPDATE
+    reactive_2 = Reactive(cas_number='7664-93-9',
+                          nature='INO',
+                          quantity=28)
+    # insert_data(reactive_2, engine)
+    # update_data('CAC-001', reactive_2, engine)

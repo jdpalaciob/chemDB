b8a904552ac249cf6783c7252693d1e75dbd7211
luis ramirez <laramirezt@unal.edu.co>
2020-09-06 12:22:18 -0500

    Create configuration for tests.

---
+++ projectchemdb/test/configuration_test.py
@@ -0,0 +1,12 @@
+DB = 'python_chem_db'
+USER = 'postgres'
+PASWRD = 'postgres'
+HOST = 'database-2.cjdum0pi52ge.us-east-1.rds.amazonaws.com'
+
+credentials = {
+    'data_base': 'python_chem_db',
+    'user_name': 'postgres',
+    'password': 'postgres',
+    'port': 5432,
+    'host': 'database-2.cjdum0pi52ge.us-east-1.rds.amazonaws.com'
+}

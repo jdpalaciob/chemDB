9aef5dc743761aa7fed852c7a414641bc4142945
luis ramirez <laramirezt@unal.edu.co>
2020-09-06 12:31:52 -0500

    Refactor the main file and the flask_block for be executed for the main file.

---
+++ projectchemdb/__main__.py
@@ -0,0 +1,4 @@
+from projectchemdb.controller import flask_block
+
+if __name__ == "__main__":
+    flask_block.run_app()
--- projectchemdb/controller/flask_block.py
+++ projectchemdb/controller/flask_block.py
@@ -2,10 +2,11 @@
 """
 
 from flask import Flask, request, Response
-import postgres_db as pdb
-from db_credentials import credentials
+from projectchemdb.persistence import postgres_db as pdb
+from projectchemdb.configuration.db_credentials import credentials
 
 app = Flask(__name__)
+
 
 @app.route('/chemDB')
 def connection():
@@ -15,6 +16,7 @@ def connection():
     pdb.stablish_connection(data_base=DB, user_name=USER, password=PASWRD)
     return Response('Access to Data Base granted', status=200)
 
+
 @app.route('/chemDB/insert', methods=['POST'])
 def insertion():
     DB = credentials['data_base']
@@ -23,15 +25,16 @@ def insertion():
 
     req_body = request.get_json()
     pdb.inserting_data(
-                       chem_id=req_body.get('chem_id', None),
-                       name=req_body.get('name', None),
-                       chem_formula=req_body.get('chem_formula', None),
-                       cas_number=req_body.get('cas_number', None),
-                       nature=req_body.get('nature', None),
-                       ph_nature=req_body.get('ph_nature'),
-                       quantity=req_body.get('quantity', None),
-                       data_base=DB, user_name=USER, password=PASWRD)
+        chem_id=req_body.get('chem_id', None),
+        name=req_body.get('name', None),
+        chem_formula=req_body.get('chem_formula', None),
+        cas_number=req_body.get('cas_number', None),
+        nature=req_body.get('nature', None),
+        ph_nature=req_body.get('ph_nature'),
+        quantity=req_body.get('quantity', None),
+        data_base=DB, user_name=USER, password=PASWRD)
     return Response(f'REACTIVE {req_body["chem_id"]} HAS BEEN INSERTED', status=200)
+
 
 @app.route('/chemDB/update', methods=['PUT'])
 def update():
@@ -41,15 +44,16 @@ def update():
 
     req_body = request.get_json()
     pdb.upgrade_data(
-                    chem_id=req_body.get('chem_id', None),
-                    name=req_body.get('name', None),
-                    chem_formula=req_body.get('chem_formula', None),
-                    cas_number=req_body.get('cas_number', None),
-                    nature=req_body.get('nature', None),
-                    ph_nature=req_body.get('ph_nature'),
-                    quantity=req_body.get('quantity', None),
-                    data_base=DB, user_name=USER, password=PASWRD)
+        chem_id=req_body.get('chem_id', None),
+        name=req_body.get('name', None),
+        chem_formula=req_body.get('chem_formula', None),
+        cas_number=req_body.get('cas_number', None),
+        nature=req_body.get('nature', None),
+        ph_nature=req_body.get('ph_nature'),
+        quantity=req_body.get('quantity', None),
+        data_base=DB, user_name=USER, password=PASWRD)
     return Response(f'REACTIVE {req_body["chem_id"]} HAS BEEN UPDATED', status=200)
+
 
 @app.route('/chemDB/delete', methods=['DELETE'])
 def delete():
@@ -59,9 +63,10 @@ def delete():
 
     req_body = request.get_json()
     pdb.delete_data(
-                    chem_id=req_body.get('chem_id', None),
-                    data_base=DB, user_name=USER, password=PASWRD)
+        chem_id=req_body.get('chem_id', None),
+        data_base=DB, user_name=USER, password=PASWRD)
     return Response(f'REACTIVE {req_body["chem_id"]} HAS BEEN DELETED', status=200)
+
 
 @app.route('/chemDB/info', methods=['GET'])
 def query():
@@ -74,8 +79,8 @@ def query():
     condition = request.args.get('equals')
 
     data = pdb.view_data(
-                view_field=view, column=column, condition_value=condition,
-                data_base=DB, user_name=USER, password=PASWRD)
+        view_field=view, column=column, condition_value=condition,
+        data_base=DB, user_name=USER, password=PASWRD)
 
     str_row = []
     for row in data:
@@ -87,7 +92,6 @@ def query():
     return Response(table, status=200)
 
 
-
-if __name__ == "__main__":
+def run_app():
     app.debug = True
     app.run()
